4c98e8ed528e5da9c70115a70ede30ff268729b2
mishrakumarashwani <mishra.kumar.ashwani@gmail.com>
2024-01-17 02:01:15 +0530

    2nd way to break singleton design pattern

--- app_java_practice/src/com/practice/Apple.java
+++ app_java_practice/src/com/practice/Apple.java
@@ -1,29 +1,27 @@
 package com.practice;
 
-public enum Apple {
-	INSTANCE;
-//	private static Apple apple;
+import java.io.Serializable;
+
+public class Apple implements Serializable {
+	private static Apple apple;
 	
 	// constructor
-//	private Apple() {
-//		if(apple != null) {
-//			throw new RuntimeException("You are trying to break singleton pattern");
-//		}
-//	}
+	private Apple() {
+		if(apple != null) {
+			throw new RuntimeException("You are trying to break singleton pattern");
+		}
+	}
 
 	// Lazy way of creating singleton object
-//	public static Apple getApple() {
-//		if(apple == null) {
-//			synchronized(Apple.class) {
-//				if(apple == null) {
-//					apple = new Apple();
-//				}
-//			}
-//		}
-//		return apple;
-//	}
-	public void test() {
-		System.out.println("test method");
+	public static Apple getApple() {
+		if(apple == null) {
+			synchronized(Apple.class) {
+				if(apple == null) {
+					apple = new Apple();
+				}
+			}
+		}
+		return apple;
 	}
 
 }
--- app_java_practice/src/com/practice/MyClass.java
+++ app_java_practice/src/com/practice/MyClass.java
@@ -1,5 +1,9 @@
 package com.practice;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
 
 public class MyClass {
@@ -18,16 +22,29 @@ public class MyClass {
 		 * 1. Reflection API to break singleton pattern
 		 * 	solution: 1-> if object is there ==> throw exception from inside constructor
 		 * 			  2 -> use enum
+		 * 
+		 * 2. Deserialization:
 		 */
 		
 //		Apple a1 = Apple.getApple();
-		Apple a1 = Apple.INSTANCE;
-		System.out.println(a1.hashCode());
-		a1.test();
+//		Apple a1 = Apple.INSTANCE;
+//		System.out.println(a1.hashCode());
+//		a1.test();
 		
-		Constructor<Apple> constructor = Apple.class.getDeclaredConstructor();
-		constructor.setAccessible(true);
-		Apple a2 = constructor.newInstance();
+//		Constructor<Apple> constructor = Apple.class.getDeclaredConstructor();
+//		constructor.setAccessible(true);
+//		Apple a2 = constructor.newInstance();
+//		System.out.println(a2.hashCode());
+		
+		Apple apple = Apple.getApple();
+		System.out.println(apple.hashCode());
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("abc.ob"));
+		oos.writeObject(apple);
+		
+		System.out.println("serialization done..");
+		
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("abc.ob"));
+		Apple a2 = (Apple) ois.readObject();
 		System.out.println(a2.hashCode());
 	}
 	
