a7c82c141b731acaa350ec1773b423e2d2a9db94
mishrakumarashwani <mishra.kumar.ashwani@gmail.com>
2024-01-17 00:52:02 +0530

    Reflection API to break singleton design pattern

--- app_java_practice/src/com/practice/MyClass.java
+++ app_java_practice/src/com/practice/MyClass.java
@@ -1,16 +1,30 @@
 package com.practice;
 
+import java.lang.reflect.Constructor;
+
 public class MyClass {
 
-	public static void main(String[] args) {
-		Apple apple1 = Apple.getApple();
-		System.out.println(apple1.hashCode());
-
-		Apple apple2 = Apple.getApple();
-		System.out.println(apple2.hashCode());
+	public static void main(String[] args) throws Exception {
+//		Apple apple1 = Apple.getApple();
+//		System.out.println(apple1.hashCode());
+//
+//		Apple apple2 = Apple.getApple();
+//		System.out.println(apple2.hashCode());
+//		
+//		System.out.println(Orange.getOrange().hashCode());
+//		System.out.println(Orange.getOrange().hashCode());
 		
-		System.out.println(Orange.getOrange().hashCode());
-		System.out.println(Orange.getOrange().hashCode());
+		/*
+		 * 1. Reflection API to break singleton pattern
+		 */
+		
+		Apple a1 = Apple.getApple();
+		System.out.println(a1.hashCode());
+		
+		Constructor<Apple> constructor = Apple.class.getDeclaredConstructor();
+		constructor.setAccessible(true);
+		Apple a2 = constructor.newInstance();
+		System.out.println(a2.hashCode());
 	}
 	
 }
